7df738c47e357a2abeb02e044a7f5e59e0ea6371
jonolimagnus <snapidog123@gmail.com>
2019-03-04 17:05:40 +0000

    sett in fyrsti partinn

---
+++ VERK_5/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, flash, redirect, request
+from flask import json
+import os
+from datetime import datetime
+from flask import render_template
+import urllib.request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST", "GET"])
+def result():
+    if request.method == "POST":
+        result = request.form
+        return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+    app.run(debug = True)
